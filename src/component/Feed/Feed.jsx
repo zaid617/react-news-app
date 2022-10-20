90e6f3829517c2f83d46fd30b1757b30b2039a8a
Zaid_ZiD <zid6177@gmail.com>
2022-10-20 22:24:28 +0500

    rightbar complete

--- src/component/Feed/Feed.jsx
+++ src/component/Feed/Feed.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
 import Post from '../post/Post'
 import "./Feed.css"
 
+
 export default function Feed() {
+
     return (
         <div className='feed'>
             <h1>Todays Headlines</h1>
             <hr />
-            <Post/>
-            <Post/>
-            <Post/>
+                <Post/>
         </div>
     )
 }
--- src/component/Rightbar/Rightbar.jsx
+++ src/component/Rightbar/Rightbar.jsx
@@ -1,21 +1,56 @@
 import React from 'react'
 import "./Rightbar.css"
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function Rightbar() {
+
+
+  let [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    let Sport = "sport"
+
+    const options = {
+      method: 'GET',
+      url: 'https://bing-news-search1.p.rapidapi.com/news/search',
+      params: { q: Sport , freshness: 'Day', textFormat: 'Raw', safeSearch: 'Off' },
+      headers: {
+        'X-BingApis-SDK': 'true',
+        'X-RapidAPI-Key': '338ccba132mshbf3b25d769906ccp10e56ejsn48743670e061',
+        'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+      }
+    };
+
+    axios.request(options).then(function (response) {
+      setData(response.data.value)
+
+
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+  }, [])
+
   return (
     <div className='rightBar'>
-      
-    <div className="rightBarbox">
-            <h2 className='mb-3'>Sports</h2>
 
-            <hr />
+      <div className="rightBarbox">
+        <h2 className='mb-3'>Sports</h2>
+
+        <hr />
 
         <div className="rightSubBox">
-            <ul>
-                <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora eveniet cupiditate pariatur eum molestias sequi fuga dolorem alias velit voluptate odit saepe suscipit corporis quasi, rem odio iure mollitia laudantium.</li>
-            </ul>
+          <ul>
+            {
+              data.map(eachpost => (
+                <li>{eachpost.name}</li>
+              ))
+            }
+          </ul>
         </div>
-    </div>
+      </div>
 
     </div>
   )
